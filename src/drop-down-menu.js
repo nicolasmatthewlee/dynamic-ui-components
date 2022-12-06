335d43a40fd801f3ce200195b2dff737696aeb8a
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-12-06 13:22:36 -0800

    setup sample module implementation

---
+++ src/drop-down-menu.js
@@ -0,0 +1,8 @@
+const DropDownMenu = () => {
+  const label = document.createElement('div');
+  label.textContent = 'test';
+
+  return label;
+};
+
+export { DropDownMenu };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log('test');
+import { DropDownMenu } from './drop-down-menu.js';
+
+const body = document.querySelector(':root');
+body.append(DropDownMenu());
