1e0234d0819a1e4d7e0a04555e449e6f9b9d189c
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-12-06 15:03:10 -0800

    added show-menu-container:hover styling with transition by color and scale

--- src/drop-down-menu.js
+++ src/drop-down-menu.js
@@ -1,8 +1,39 @@
-const DropDownMenu = () => {
-  const label = document.createElement('div');
-  label.textContent = 'test';
+import './drop-down-menu.css';
 
-  return label;
+import DROP_DOWN_ICON from './assets/drop-down.svg';
+
+const createElement = (type, className, content = '') => {
+  let element = document.createElement(type);
+  element.setAttribute('class', className);
+  if (type == 'img') {
+    element.src = content;
+  } else {
+    element.textContent = content;
+  }
+  return element;
+};
+
+const DropDownMenu = (sections) => {
+  const menu = createElement('div', 'drop-down-menu');
+
+  const showMenuContainer = createElement('button', 'show-menu-container');
+  showMenuContainer.addEventListener('click', () => {
+    sectionsContainer.classList.toggle('active');
+  });
+  const menuLabel = createElement('div', 'show-menu-label', 'Menu');
+  const showMenuButton = createElement('img', 'show-menu-icon', DROP_DOWN_ICON);
+  showMenuContainer.append(menuLabel, showMenuButton);
+
+  const sectionsContainer = createElement('div', 'sections-container');
+
+  for (let name of sections) {
+    let section = createElement('div', 'section', name);
+    sectionsContainer.append(section);
+  }
+
+  menu.append(showMenuContainer, sectionsContainer);
+
+  return menu;
 };
 
 export { DropDownMenu };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import './reset.css';
+
 import { DropDownMenu } from './drop-down-menu.js';
 
 const body = document.querySelector(':root');
-body.append(DropDownMenu());
+body.append(DropDownMenu(['Products', 'Pricing', 'Developers', 'About']));
