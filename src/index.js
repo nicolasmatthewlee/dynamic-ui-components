d32ab73ad8be996453ab5377be0732c364fa82c2
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-12-07 10:56:05 -0800

    increased padding from 20px to 30px around menu overlay and reconfigured drop-down-menu to be used as an inline-element flexibly rather than in a set top-right position

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+// this is an example implementation of the components
+// in this module
+
 import './reset.css';
 
 import { DropDownMenu } from './drop-down-menu.js';
 
 const body = document.querySelector(':root');
 
-body.append(
+let header = document.createElement('div');
+header.style.display = 'flex';
+header.style.gap = '30px';
+header.style.backgroundColor = 'red';
+header.style.padding = '30px 0px 30px 30px';
+body.append(header);
+
+header.append(
   DropDownMenu(
     ['Products', 'Pricing', 'Developers', 'About'],
     [
@@ -16,7 +26,7 @@ body.append(
   )
 );
 
-body.append(
+header.append(
   DropDownMenu(
     ['Products', 'Pricing', 'Developers', 'About'],
     [
@@ -28,7 +38,7 @@ body.append(
   )
 );
 
-body.append(
+header.append(
   DropDownMenu(
     ['Products', 'Pricing', 'Developers', 'About'],
     [
