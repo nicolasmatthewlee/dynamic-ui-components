f88d8ecad41b53084d72597ed01460e71ad026c1
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-12-06 22:18:58 -0800

    implemented and styled hide-menu-button with transitions

--- src/drop-down-menu.js
+++ src/drop-down-menu.js
@@ -1,6 +1,7 @@
 import './drop-down-menu.css';
 
-import DROP_DOWN_ICON from './assets/drop-down.svg';
+import EXPAND_MORE_ICON from './assets/expand-more.svg';
+import EXPAND_LESS_ICON from './assets/expand-less.svg';
 
 const createElement = (type, className, content = '') => {
   let element = document.createElement(type);
@@ -16,19 +17,45 @@ const createElement = (type, className, content = '') => {
 const DropDownMenu = (sections) => {
   const menu = createElement('div', 'drop-down-menu');
 
+  //   show menu
   const showMenuContainerBackdrop = createElement(
     'div',
     'show-menu-container-backdrop'
   );
   const showMenuContainer = createElement('button', 'show-menu-container');
   showMenuContainer.addEventListener('click', () => {
-    sectionsContainer.classList.toggle('active');
+    menuOverlay.classList.toggle('active');
   });
   const menuLabel = createElement('div', 'show-menu-label', 'Menu');
-  const showMenuButton = createElement('img', 'show-menu-icon', DROP_DOWN_ICON);
+  const showMenuButton = createElement(
+    'img',
+    'show-menu-icon',
+    EXPAND_MORE_ICON
+  );
   showMenuContainer.append(menuLabel, showMenuButton);
   showMenuContainerBackdrop.append(showMenuContainer);
 
+  const menuOverlay = createElement('div', 'menu-overlay');
+
+  //   hide menu
+  const hideMenuContainer = createElement('button', 'hide-menu-container');
+  const hideMenuButtonContainer = createElement(
+    'div',
+    'hide-menu-button-container'
+  );
+  const hideMenuButton = createElement(
+    'img',
+    'hide-menu-button',
+    EXPAND_LESS_ICON
+  );
+  hideMenuContainer.addEventListener('click', () => {
+    menuOverlay.classList.toggle('active');
+  });
+  hideMenuButtonContainer.append(hideMenuButton);
+  hideMenuContainer.append(hideMenuButtonContainer);
+  menuOverlay.append(hideMenuContainer);
+
+  // menu content
   const sectionsContainer = createElement('div', 'sections-container');
 
   for (let name of sections) {
@@ -36,7 +63,9 @@ const DropDownMenu = (sections) => {
     sectionsContainer.append(section);
   }
 
-  menu.append(showMenuContainerBackdrop, sectionsContainer);
+  menuOverlay.append(sectionsContainer);
+
+  menu.append(showMenuContainerBackdrop, menuOverlay);
 
   return menu;
 };
